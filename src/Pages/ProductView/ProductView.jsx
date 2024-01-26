c6743192be4f5645e0bd507d2bacd6a52f508af4
Andre Ignorosa <andreiglu@hotmail.com>
2024-01-26 17:12:54 -0600

    ProductView.jsx - Sección de faq agregada y con la lógica de mostrar el contenido

--- src/Pages/ProductView/ProductView.jsx
+++ src/Pages/ProductView/ProductView.jsx
@@ -3,12 +3,20 @@ import { AddToCartForm } from '../../UI/components';
 import { ScrollRestoration, useParams } from 'react-router-dom';
 import './ProductView.css';
 import { getItemByProductNumber } from '../../helpers/getItemByProductNumber';
+import { useEffect, useState } from 'react';
 
 export const ProductView = () => {
   
   const { productNumber } = useParams();
 
+  const [ faqOption, setFaqOption ] = useState( false );
 
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setFaqOption(!faqOption);
+  //   }, 10); // Ajusta el tiempo según sea necesario
+  //   return () => clearTimeout(timer);
+  // }, [faqOption]);
   
 
   const item = getItemByProductNumber( productNumber );
@@ -19,6 +27,7 @@ export const ProductView = () => {
           <section className='product-view section'>
               <div className="container">
                 <div className="product-wrapper grid">
+                  
                   <div className="product-image-container">
                     <img src={`/assets/${ item.genre }/${ item.productName }.jpg`} alt={item.description} />
                   </div>
@@ -41,7 +50,72 @@ export const ProductView = () => {
                   
           <section className='faq-product-section section'>
             <div className="container">
+                  <div className="faq-wrapper">
+                    <div className="faq-header">
+                      <button className= {`btn ${ ( !faqOption ) ? 'active' : ''}`} onClick={ () => setFaqOption( false )}>Details & Specs</button>
+                      <button className={`btn ${ ( faqOption ) ? 'active' : ''}`} onClick={ () => setFaqOption( true)}>Faq</button>
+                    </div>
+
+                    <div className={`details-content animate__animated animate__fadeIn animate__faster`}
+                         style={{ display: ( !faqOption ) ? '' : 'none'}} 
+                    >
+                        <h3>Best Product 2019</h3>
+                            <p>
+                            As seen on Techcrunch, Engadget and many more...!‍ <br/><br/>
+                            A shared love for good design, outdoor adventure and a home base in the state of Oregon fueled the desire for us to work together with Salomon on the Super 8 Snowboard and Chapter collaboration. What began as preliminary notes from a call to meld shared values and likeminded interest soon transformed into a limited edition version of the knife that started it all.
+                            </p>
+                            <h4>Features</h4>
+                            <ul>
+                              <li>World class materials</li>
+                              <li>Great looking product</li>
+                              <li>Designed in USA</li>
+                            </ul>
+                            <p>When you buy this product you are supporting hard working families.</p>
+                      </div> 
+
+                      <div className={`faq-content animate__animated animate__fadeIn animate__faster`}
+                          style={{ display: ( faqOption ) ? '' : 'none'}}
+                      >
+                          <p><b>Q:</b> Here goes your question?</p> 
+                          <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                          <p><b>Q:</b> Here goes your question?</p> 
+                          <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                          <p><b>Q:</b> Here goes your question?</p> 
+                          <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                      </div>
+
+
+                    {/* {
+                      ( !faqOption )
+                            ? <div className={`details-content ${ (!faqOption ) ? 'animate__animated animate__fadeIn animate__faster' : ''}`}>
+                                <h3>Best Product 2019</h3>
+                                    <p>
+                                    As seen on Techcrunch, Engadget and many more...!‍ <br/><br/>
+                                    A shared love for good design, outdoor adventure and a home base in the state of Oregon fueled the desire for us to work together with Salomon on the Super 8 Snowboard and Chapter collaboration. What began as preliminary notes from a call to meld shared values and likeminded interest soon transformed into a limited edition version of the knife that started it all.
+                                    </p>
+                                    <h4>Features</h4>
+                                    <ul>
+                                      <li>World class materials</li>
+                                      <li>Great looking product</li>
+                                      <li>Designed in USA</li>
+                                    </ul>
+                                    <p>When you buy this product you are supporting hard working families.</p>
+                                </div> 
+                              : <div className={`faq-content ${ ( faqOption ) ? 'animate__animated animate__fadeIn animate__faster' : '' }`}>
+                                  <p><b>Q:</b> Here goes your question?</p> 
+                                  <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                                  <p><b>Q:</b> Here goes your question?</p> 
+                                  <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                                  <p><b>Q:</b> Here goes your question?</p> 
+                                  <p><b>A:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic molestias maiores eligendi delectus esse.</p> 
+                                </div>
+                    } */}
+
                   
+
+                    
+
+                  </div>
             </div>
           </section>
         
