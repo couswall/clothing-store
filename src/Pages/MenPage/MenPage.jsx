ffc8a9f944ae0d8e0e01351fe6bbec8219dcad55
Andre Ignorosa <andreiglu@hotmail.com>
2024-01-30 10:43:50 -0600

    hero-men-pag.jpg agregada

--- src/Pages/MenPage/MenPage.jsx
+++ src/Pages/MenPage/MenPage.jsx
@@ -1,9 +1,25 @@
+import heroImage from '/assets/hero-men-page.jpg';
+
+import './MenPage.css';
 
 export const MenPage = () => {
   return (
     <>
-        <h1>Men Page</h1>
-        <hr />
+        <section className='hero-men'
+            style={{
+                backgroundImage: `url(${heroImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
+          <article className='hero-content container flex'>
+            <h1 className='text-center'>Men Collection</h1>
+            <div className="underline-title"></div>
+            <p className='sm text-center'>Mid-Season SALE only at Olea</p>
+        </article>
+        <div className="overlay-hero"></div>
+        </section>
     </>
     )
 }
