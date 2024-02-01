aacccca2cd9be41ecc984cc9093f54ac5be00e07
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-01 11:46:25 -0600

    ContactPage.jsx - hero y features agregados

--- src/Pages/Contact/ContactPage.jsx
+++ src/Pages/Contact/ContactPage.jsx
@@ -1,10 +1,28 @@
+import heroContactImage from "/assets/contact-hero.jpg";
+import './ContactPage.css';
+import { Features } from "../../UI/components/Features/Features";
 
 export const ContactPage = () => {
   return (
 
     <>
-        <h1>ContactPage</h1>
-        <hr />
+        <section 
+          className="hero-contact section"
+          style={{
+            backgroundImage: `url(${heroContactImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center bottom',
+            backgroundRepeat: 'no-repeat',
+      }}  
+        >
+          <article className='hero-content container flex'>
+            <h1 className='text-center'>Contact us</h1>
+            <div className="underline-title"></div>
+          </article>
+        <div className="overlay-hero"></div>
+        </section>
+
+        <Features/>
     </>
   )
 }
