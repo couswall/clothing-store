379753e9564c958b6d37308bd0e10100d68c3e8d
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-01 12:27:48 -0600

    ContactPage.jsx - useForm agregado

--- src/Pages/Contact/ContactPage.jsx
+++ src/Pages/Contact/ContactPage.jsx
@@ -1,8 +1,20 @@
 import heroContactImage from "/assets/contact-hero.jpg";
 import './ContactPage.css';
 import { Features } from "../../UI/components/Features/Features";
+import { useForm } from "../../hooks/useForm";
 
 export const ContactPage = () => {
+
+  const { name, email, text, onInputChange, onResetForm } = useForm({
+    name: '',
+    email: '',
+    text: ''
+  })
+
+  const onSubmitContact = (e) => {
+    e.preventDefault();
+  }
+
   return (
 
     <>
@@ -22,7 +34,48 @@ export const ContactPage = () => {
         <div className="overlay-hero"></div>
         </section>
 
-        <Features/>
+        {/* <Features/> */}
+
+        <section className="contact-section section">
+          <div className="container grid">
+            <div className="contact-text-container">
+              <h2>Get in touch with our support team</h2>
+              <p>With our versatile product range and commitment to affordability, we are here to help you go places in style, without any compromises. So why not browse our collections today and let us help you achieve your fashion goals with ease!
+              </p>
+            </div>
+
+            <form className="contact-us-form" onSubmit={ onSubmitContact }>
+            <input 
+              type="text" 
+              placeholder="Your name" 
+              required 
+              name="text"
+              value={text}
+              onInput={ onInputChange }
+            />
+            <input 
+              type="email" 
+              placeholder="Your email"
+              required
+              name="email"
+              value={email}
+              onInput={ onInputChange }
+            />
+            <textarea placeholder="Your text"/>
+
+            <button 
+              className="btn btn-secondary" 
+              type="submit"
+              name="text"
+              value={text}
+              onInput={ onInputChange }
+            >
+              Submit
+            </button>
+          </form>
+
+          </div>
+        </section>
     </>
   )
 }
