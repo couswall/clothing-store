afa03c30637fd78c215b45ac9bb610dcbab66060
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-05 13:28:26 -0600

    CartProvider.jsx - showSearchInput state creado

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -10,10 +10,13 @@ export const CartProvider = ({ children }) => {
 
     // showCart 
     const [showCart, setShowCart] = useState(false);
+
+    //show SearchInput
+    const [showSearchInput, setShowSearchInput] = useState( false );
   
     return (
     
-    <CartContext.Provider value={{ cart, onAddItem, onDeleteItem, showCart, setShowCart }}> 
+    <CartContext.Provider value={{ cart, onAddItem, onDeleteItem, showCart, setShowCart, showSearchInput, setShowSearchInput }}> 
         { children }
     </CartContext.Provider>
     
