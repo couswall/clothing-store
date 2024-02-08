ade30bcdc9aa20fc552188beb3cec50c036d8768
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-08 13:39:37 -0600

    AccesoriesPage.jsx - CategoryProduct.jsx agregado con sus props definidas

--- src/Pages/AccesoriesPage/AccesoriesPage.jsx
+++ src/Pages/AccesoriesPage/AccesoriesPage.jsx
@@ -1,8 +1,29 @@
 
+import { CategoryProduct } from '../../UI/components/CategoryProduct/CategoryProduct'
+import { items } from '../../data/data'
 import './AccesoriesPage.css'
 
 export const AccesoriesPage = () => {
+
+    const itemList = items.filter( item => item.category === 'Accesories' ); 
+    const arrayOfCategories = ["All", "Women", "Men"];
+   
+    const filterItems = ( itemsList = [] , name = '', value = '' ) => {
+        if ( name === "All" || value === "All" ) {
+          return itemsList
+        }else{
+         return itemsList.filter(  item => item.genre === name.toLowerCase() || item.genre === value.toLowerCase()  );
+        }
+    }
+
   return (
-    <div>AccesoriesPage</div>
-  )
+    <>
+
+        <CategoryProduct 
+            itemsList={ itemList }
+            arrayOfCategories={ arrayOfCategories }
+            filterItems={ filterItems }
+        />
+    </>
+    )
 }
