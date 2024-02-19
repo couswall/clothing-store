f871e09b057ab24fbc32eb3f1244b8a6b0f94eb9
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-19 17:55:54 -0600

    CallToAction.jsx - LazyBacground component añadido

--- src/Pages/Homepage/components/CallToAction/CallToAction.jsx
+++ src/Pages/Homepage/components/CallToAction/CallToAction.jsx
@@ -1,29 +1,21 @@
 import  callToActionImage  from '/assets/best-day-call2action.jpg';
 import { Link } from "react-router-dom";
 import './CallToAction.css'
+import { LazyBackgroundImg } from '../../../../UI/components/LazyBackgroundImg/LazyBackgroundImg';
 
 
 export const CallToAction = () => {
   return (
     <>
-        <section 
-        className="best-day-call2action section"
-        style={{
-          backgroundImage: `url(${ callToActionImage })`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat'
-        }}
-      >
 
-        <article className='hero-content container flex'>
-            <h2 className='text-center'>Best of the day</h2>
-            <Link className='sm' to="/new-collection">View Collection</Link>
-        </article>
+        <LazyBackgroundImg imgUrl={ callToActionImage } cssClass='best-day-call2action'>
+          <article className='hero-content container flex'>
+              <h2 className='text-center'>Best of the day</h2>
+              <Link className='sm' to="/new-collection">View Collection</Link>
+          </article>
+          <div className="overlay-hero"></div>
+        </LazyBackgroundImg>
 
-        <div className="overlay-hero"></div>
-
-      </section>
     </>
   )
 }
