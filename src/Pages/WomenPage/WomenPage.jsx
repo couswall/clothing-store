b035faeb69dbe65fdbd373585b4fad9baf34800d
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-19 17:32:34 -0600

    WomenPage.jsx - LazyBackgroundImage agregado

--- src/Pages/WomenPage/WomenPage.jsx
+++ src/Pages/WomenPage/WomenPage.jsx
@@ -5,6 +5,7 @@ import heroImage from '/assets/hero-women.jpg';
 import { Features } from '../../UI/components/Features/Features';
 import "./WomenPage.css";
 import { getCategories } from '../../helpers/getCategories';
+import { LazyBackgroundImg } from '../../UI/components/LazyBackgroundImg/LazyBackgroundImg';
 
 export const WomenPage = () => {
 
@@ -20,21 +21,16 @@ export const WomenPage = () => {
   
   return (
     <>
-    <section className='hero-women'
-            style={{
-                backgroundImage: `url(${heroImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-            }}
-        >
-          <article className='hero-content container flex'>
-            <h1 className='text-center'>Women Collection</h1>
-            <div className="underline-title"></div>
-            <p className='sm text-center'>Mid-Season SALE only at Olea</p>
-        </article>
-        <div className="overlay-hero"></div>
-        </section>
+
+    <LazyBackgroundImg imgUrl={ heroImage } cssClass='hero-women'>
+        <article className='hero-content container flex'>
+              <h1 className='text-center'>Women Collection</h1>
+              <div className="underline-title"></div>
+              <p className='sm text-center'>Mid-Season SALE only at Olea</p>
+          </article>
+          <div className="overlay-hero"></div>
+    </LazyBackgroundImg>
+
         
         <CategoryProduct 
           itemsList = { itemsList } 
