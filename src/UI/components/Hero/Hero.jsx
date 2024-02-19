a9381df3af00c6066c873764cf36cbd606a3a904
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-19 17:59:16 -0600

    Hero.jsx - LazyBacground component añadido

--- src/UI/components/Hero/Hero.jsx
+++ src/UI/components/Hero/Hero.jsx
@@ -1,29 +1,21 @@
 import heroImage from '/assets/clothing-store-hero.jpg';
 import { Link } from "react-router-dom";
 import './Hero.css';
+import { LazyBackgroundImg } from '../LazyBackgroundImg/LazyBackgroundImg';
 
 export const Hero = () => {
   return (
     <>
-        <section 
-            className='hero-section'
-            style={{
-                backgroundImage: `url(${heroImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: '100% 25%',
-                backgroundRepeat: 'no-repeat',
-            }}
-        >
-        <article className='hero-content container flex'>
-            <h1 className='text-center'>Dollek Collection on Sale Now</h1>
-            <div className="underline-title"></div>
-            <p className='sm text-center'>Mid-Season SALE only at Olea</p>
-            <Link className='sm' to="/new-collection">View Collection</Link>
-        </article>
 
-
-        <div className="overlay-hero"></div>
-        </section>
+        <LazyBackgroundImg imgUrl={ heroImage } cssClass='hero-section' imgPosition='100% 25%'>
+            <article className='hero-content container flex'>
+                <h1 className='text-center'>Dollek Collection on Sale Now</h1>
+                <div className="underline-title"></div>
+                <p className='sm text-center'>Mid-Season SALE only at Olea</p>
+                <Link className='sm' to="/new-collection">View Collection</Link>
+            </article>
+            <div className="overlay-hero"></div>
+        </LazyBackgroundImg>
     </>
   )
 }
